ef8cc2effa421d4799c5795b36c6d11939f8d209
siddhant <siddhanjain935@gmail.com>
2022-04-29 18:25:50 +0530

    29 Apr 2022 Vertex -:  event loops and deployment methods and worker threads and communications types and event bus basic understanding with communications

--- src/main/java/com/personal/javafilestwo/vertex/eventloops/WorkerThreadExample.java
+++ src/main/java/com/personal/javafilestwo/vertex/eventloops/WorkerThreadExample.java
@@ -1,18 +1,50 @@
 package com.personal.javafilestwo.vertex.eventloops;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Promise;
 import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 public class WorkerThreadExample extends AbstractVerticle {
+
+    private static final Logger LOG = (Logger) LoggerFactory.getLogger(WorkerThreadExample.class);
+
     public static void main(String[] args) {
-        var ve= Vertx.vertx();
+        var ve = Vertx.vertx();
         ve.deployVerticle("com.personal.javafilestwo.vertex.eventloops.WorkerThreadExample");
     }
 
     @Override
     public void start(Promise<Void> startPromise) throws Exception {
+        vertx.deployVerticle("com.personal.javafilestwo.vertex.eventloops.WorkerVerticle",
+                new DeploymentOptions()
+                        .setWorker(true)
+                        .setWorkerPoolSize(1)
+                        .setWorkerPoolName("my worker verticle"));
         startPromise.complete();
+        executeBlockingCode();
+    }
+
+    private void executeBlockingCode() {
+        vertx.executeBlocking(event -> {
+            try {
+                Thread.sleep(5000);
+                event.complete();
+            } catch (InterruptedException e) {
+                LOG.debug("failed ",e);
+                event.fail(e);
+            }
+        }, result -> {
+
+            if (result.succeeded()) {
+                LOG.debug("succeed :","Executing Blocking call done");
+            } else {
+                LOG.debug("failed : ", result.cause());
+            }
+        });
 
     }
 }
--- src/main/java/com/personal/javafilestwo/vertex/verticles/MainVerticle.java
+++ src/main/java/com/personal/javafilestwo/vertex/verticles/MainVerticle.java
@@ -1,9 +1,13 @@
 package com.personal.javafilestwo.vertex.verticles;
 
+import com.personal.javafilestwo.vertex.eventloops.WorkerThreadExample;
 import io.vertx.core.*;
 import io.vertx.core.json.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle{
+    private static final Logger LOG = (Logger) LoggerFactory.getLogger(MainVerticle.class);
     public static void main(String[] args) {
         final Vertx vertx =Vertx.vertx();
         vertx.deployVerticle(new MainVerticle());
@@ -11,7 +15,7 @@ public class MainVerticle extends AbstractVerticle{
 
     @Override
     public void start(final Promise<Void> startPromise) throws Exception {
-        System.out.println("start :"+getClass().getName());
+        LOG.debug("Start {}",getClass().getName());
         for(int i =0 ; i<10 ;i++){
             vertx.deployVerticle("com.personal.javafilestwo.vertex.verticles.SubVerticle1",new DeploymentOptions().setConfig(new JsonObject().put("i",i)));
         }
