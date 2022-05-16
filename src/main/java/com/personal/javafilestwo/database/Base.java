df29e5950e212e5bf3615b87e20572ae6a373030
siddhant <siddhanjain935@gmail.com>
2022-05-16 21:07:16 +0530

    database

--- src/main/java/com/personal/javafilestwo/database/Base.java
+++ src/main/java/com/personal/javafilestwo/database/Base.java
@@ -1,9 +1,6 @@
 package com.personal.javafilestwo.database;
 
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Base {
 //    static String CREATE_TABLE_EMP ="CREATE TABLE IF NOT EXISTS EMPLOYEE(Employee_id int,Name varchar(255),City varchar(255), DOB DATE,Department_id int)";
@@ -13,13 +10,21 @@ public class Base {
    static String QUERY="Select * from employees left outer join departments on employees.department_id=departments.department_id;";
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        var connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", "siddhant", "Sid@mtdt#25");
+        var connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "siddhant", "Sid@mtdt#25");
         Statement smt = connection.createStatement();
+        smt.execute("use nms");
         try {
             //creations of table
 //            smt.execute(CREATE_TABLE_EMP);
 //            smt.execute(CREATE_TABLE_DEP);
             ResultSet res = smt.executeQuery(QUERY);
+            ResultSetMetaData rsmd = res.getMetaData();
+            int columns = rsmd.getColumnCount();
+            int i =1;
+            while(i<columns){
+                System.out.println(rsmd.getColumnName(i));
+                i++;
+            }
             while(res.next())
                 System.out.println(res.getInt(1)+"  "+res.getString(2)+"  "+res.getString(3));
             connection.close();
